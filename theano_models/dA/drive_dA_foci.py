209b99948c4afb460155e523c06373c2a9cb5f6f
Lee Zamparo <zamparo@gmail.com>
2013-04-29 17:58:57 -0400

    Altered AutoEncoder so that the loss function can be specified, allowing it to be used for different types of data than just binary. Added a drive_dA_foci script to test out the new AutoEncoder on the foci data, and a matching jobscript for submitting to the arc queue on scinet Cleaned up load_shared so that any arithmetic does not happen inside slicing.

---
+++ theano_models/dA/drive_dA_foci.py
@@ -0,0 +1,150 @@
+# Try out the denoising Autoencoder on foci data 
+import numpy
+
+import theano
+import theano.tensor as T
+from AutoEncoder import AutoEncoder
+from theano.tensor.shared_randomstreams import RandomStreams
+
+from utils.extract_datasets import extract_labeled_chunkrange
+from utils.load_shared import load_data_labeled
+from tables import *
+
+import os
+import sys
+import time
+from datetime import datetime
+from optparse import OptionParser
+
+
+def drive_dA(learning_rate=0.1, training_epochs=15,
+             batch_size=20):
+    """
+        This dA is driven with foci data
+    
+        :type learning_rate: float
+        :param learning_rate: learning rate used for training the DeNosing
+                              AutoEncoder
+    
+        :type training_epochs: int
+        :param training_epochs: number of epochs used for training
+    
+        :type batch_size: int
+        :param batch_size: size of each minibatch
+    
+    """
+    parser = OptionParser()
+    parser.add_option("-d", "--dir", dest="dir", help="test output directory")
+    parser.add_option("-c", "--corruption", dest="corruption", help="use this amount of corruption for the denoising AE")
+    parser.add_option("-i", "--inputfile", dest="inputfile", help="the hdf5 filename as an absolute pathname")
+    (options, args) = parser.parse_args()    
+
+    current_dir = os.getcwd()    
+
+    os.chdir(options.dir)
+    today = datetime.today()
+    day = str(today.date())
+    hour = str(today.time())
+    output_filename = "denoising_autoencoder_mnist." + day + "." + hour
+    output_file = open(output_filename,'w')
+    
+    print >> output_file, "Run on " + str(datetime.now())    
+    
+    os.chdir(current_dir)
+    
+    data_set_file = openFile(str(options.inputfile), mode = 'r')
+    datafiles, labels = extract_labeled_chunkrange(data_set_file, num_files = 10)
+    datasets = load_data_labeled(datafiles, labels)
+    train_set_x, train_set_y = datasets[0]
+
+    # compute number of minibatches for training, validation and testing
+    n_train_batches = train_set_x.get_value(borrow=True).shape[0] / batch_size
+
+    # allocate symbolic variables for the data
+    index = T.lscalar()    # index to a [mini]batch
+    x = T.matrix('x')  # the data matrix
+    
+    ####################################
+    # BUILDING THE MODEL NO CORRUPTION #
+    ####################################
+
+    rng = numpy.random.RandomState(123)
+    theano_rng = RandomStreams(rng.randint(2 ** 30))
+
+    da = AutoEncoder(numpy_rng=rng, theano_rng=theano_rng, input=x,
+            n_visible=train_set_x.shape[1], n_hidden=400, loss='squared')
+
+    cost, updates = da.get_cost_updates(corruption_level=0.,
+                                        learning_rate=learning_rate)
+
+    train_da = theano.function([index], cost, updates=updates,
+         givens={x: train_set_x[index * batch_size:
+                                (index + 1) * batch_size]})
+
+    start_time = time.clock()
+
+    ############
+    # TRAINING #
+    ############
+
+    # go through training epochs
+    for epoch in xrange(training_epochs):
+        # go through training set
+        c = []
+        for batch_index in xrange(n_train_batches):
+            c.append(train_da(batch_index))
+
+        print >> output_file, 'Training epoch %d, cost ' % epoch, numpy.mean(c)
+
+    end_time = time.clock()
+
+    training_time = (end_time - start_time)
+
+    print >> output_file, ('The 0 corruption code for file ' +
+                          os.path.split(__file__)[1] +
+                          ' ran for %.2fm' % ((training_time) / 60.))    
+    
+            
+    ##########
+    # Build the model, with corruption 
+    ##########
+    
+    rng = numpy.random.RandomState(123)
+    theano_rng = RandomStreams(rng.randint(2 ** 30))
+ 
+    da = AutoEncoder(numpy_rng=rng, theano_rng=theano_rng, input=x,
+                n_visible=train_set_x.shape[1], n_hidden=400, loss='squared')    
+
+    cost, updates = da.get_cost_updates(corruption_level=float(options.corruption),
+                                        learning_rate=learning_rate)
+
+    train_da = theano.function([index], cost, updates=updates,
+         givens={x: train_set_x[index * batch_size:
+                                (index + 1) * batch_size]})
+
+    start_time = time.clock()
+    
+    ##########
+    # Train the model
+    ##########
+    # go through training epochs
+    for epoch in xrange(training_epochs):
+        # go through trainng set
+        c = []
+        for batch_index in xrange(n_train_batches):
+            c.append(train_da(batch_index))
+
+        print >> output_file, 'Training epoch %d, cost ' % epoch, numpy.mean(c)
+
+    end_time = time.clock()
+
+    training_time = (end_time - start_time)
+
+    print >> output_file, ('The ' + str(options.corruption) + '% corruption code for file ' +
+                          os.path.split(__file__)[1] +
+                          ' ran for %.2fm' % ((training_time) / 60.))
+
+    output_file.close()
+    
+if __name__ == '__main__':
+    drive_dA()    
