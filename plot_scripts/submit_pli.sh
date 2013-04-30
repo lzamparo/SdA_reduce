7b03b6632a3ace851db3c219fbb57e46dcccb4cf
Lee Zamparo <zamparo@gmail.com>
2013-04-30 00:55:06 -0400

    Moved plotting scripts into their own folder, added a job submission script for one of them.

---
+++ plot_scripts/submit_pli.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Torque submission script for SciNet GPC
+#
+# N.B: PBS lines are interpreted by qsub.  Change these defaults as 
+# required
+#
+#PBS -l nodes=1:ppn=8,walltime=3:00:00
+#PBS -N plot_pli
+
+# Load theano modules
+#cd ~/jobscripts
+#sh load_theano_modules.sh
+
+# Run the job
+cd $PBS_O_WORKDIR
+python plot_pca_lle_isomap.py --h5file "${SCRATCH}/sm_rep1_data/sample.h5" --size 7 --sample-size 3000 --dimension 2 --output "${SCRATCH}/figures" 
