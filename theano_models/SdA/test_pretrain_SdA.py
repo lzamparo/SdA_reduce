d1db12bdd3a0c01143db08dc8b81dba8038e9ef5
Lee Zamparo <zamparo@gmail.com>
2013-05-17 18:39:00 -0400

    Removed call to reconstruct_state in test_pretrain_SdA.py

--- theano_models/SdA/test_pretrain_SdA.py
+++ theano_models/SdA/test_pretrain_SdA.py
@@ -43,7 +43,7 @@ def test_pickled_SdA(num_epochs=10, pretrain_lr=0.001, batch_size=10):
     today = datetime.today()
     day = str(today.date())
     hour = str(today.time())
-    output_filename = "test_stacked_denoising_autoencoder_pretrain." + day + "." + hour
+    output_filename = "test_pickled_stacked_denoising_autoencoder_pretrain." + day + "." + hour
     output_file = open(output_filename,'w')
     os.chdir(current_dir)    
     print >> output_file, "Run on " + str(datetime.now())    
@@ -107,10 +107,79 @@ def test_pickled_SdA(num_epochs=10, pretrain_lr=0.001, batch_size=10):
     pickled_sda = cPickle.load(f)
     f.close()    
     
-    pickled_sda.reconstruct_layers(x)
+    # Train for the remaining 
+    pretraining_fns = pickled_sda.pretraining_functions(train_set_x=train_set_x,
+                                                    batch_size=batch_size)
+    
+    print >> output_file, 'Resume training...'
+    start_time = time.clock()
+    ## Pre-train layer-wise
+    corruption_levels = [float(options.corruption), float(options.corruption), float(options.corruption)]
+    for i in xrange(pickled_sda.n_layers):
+        # pickle the current model, along with the current epoch run
+        # TODO: pickling code here.
+        
+        for epoch in xrange(num_epochs / 2):
+            # go through the training set
+            c = []
+            for batch_index in xrange(n_train_batches):
+                c.append(pretraining_fns[i](index=batch_index,
+                         corruption=corruption_levels[i],
+                         lr=pretrain_lr))
+            print >> output_file, 'Pre-training layer %i, epoch %d, cost ' % (i, epoch),
+            print >> output_file, numpy.mean(c)
+
+    end_time = time.clock()    
+    print >> output_file, ('Pretraining time for file ' +
+                          os.path.split(__file__)[1] +
+                          ' was %.2fm to go through the remaining %i epochs' % (((end_time - start_time) / 60.), (num_epochs / 2)))
+       
+    output_file.close()
+    
+    
+def test_unpickled_SdA(num_epochs=10, pretrain_lr=0.001, batch_size=10):
+    """ Unpickle an SdA from file, continue pre-training for a given number of epochs.
+    
+    :type num_epochs: int
+    :param num_epochs: number of epoch to do pretraining
+
+    :type pretrain_lr: float
+    :param pretrain_lr: learning rate to be used during pre-training
+
+    :type batch_size: int
+    :param batch_size: train in mini-batches of this size  
+    """
+    
+    current_dir = os.getcwd()    
+    
+    os.chdir(options.dir)
+    today = datetime.today()
+    day = str(today.date())
+    hour = str(today.time())
+    output_filename = "test_unpickled_stacked_denoising_autoencoder_pretrain." + day + "." + hour
+    output_file = open(output_filename,'w')
+    os.chdir(current_dir)    
+    print >> output_file, "Run on " + str(datetime.now())    
+    
+    # Get the training data sample from the input file
+    data_set_file = openFile(str(options.inputfile), mode = 'r')
+    datafiles = extract_unlabeled_chunkrange(data_set_file, num_files = 10)
+    train_set_x = load_data_unlabeled(datafiles)
+    data_set_file.close()
+
+    # compute number of minibatches for training, validation and testing
+    n_train_batches, n_features = train_set_x.get_value(borrow=True).shape
+    n_train_batches /= batch_size    
+    
+    # Unpickle the SdA
+    print >> output_file, 'Unpickling the model...'
+    f = file(options.savefile, 'rb')
+    pickled_sda = cPickle.load(f)
+    f.close()    
+    
+    #pickled_sda.reconstruct_layers(x)
     
     # Train for the remaining 
-    print >> output_file, 'Resume training...'
     pretraining_fns = pickled_sda.pretraining_functions(train_set_x=train_set_x,
                                                     batch_size=batch_size)
     
@@ -138,6 +207,7 @@ def test_pickled_SdA(num_epochs=10, pretrain_lr=0.001, batch_size=10):
                           ' was %.2fm to go through the remaining %i epochs' % (((end_time - start_time) / 60.), (num_epochs / 2)))    
     output_file.close()
     
+    
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("-d", "--dir", dest="dir", help="test output directory")
@@ -149,3 +219,6 @@ if __name__ == '__main__':
     (options, args) = parser.parse_args()        
     
     test_pickled_SdA()
+    test_unpickled_SdA()
+    
+    
