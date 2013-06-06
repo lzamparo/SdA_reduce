cc6aefb4cb2ff5dade09058418ba217c7122bb8c
Lee Zamparo <zamparo@gmail.com>
2013-06-06 19:18:42 -0400

    Removed explicit assignment of theano to gpu, see if I still encounter the TypeError bug.

--- theano_models/SdA/test_mom_wd_SdA.py
+++ theano_models/SdA/test_mom_wd_SdA.py
@@ -1,9 +1,6 @@
 import numpy
 import cPickle
 
-import theano.sandbox.cuda
-theano.sandbox.cuda.use('gpu')
-
 import theano
 import theano.tensor as T
 from mlp.logistic_sgd import LogisticRegression
