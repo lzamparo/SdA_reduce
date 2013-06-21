8bcadd8700e85e75a8e57bd19912717afb9ddca8
Lee Zamparo <zamparo@gmail.com>
2013-06-21 17:26:37 -0400

    Added a new directory for output of hyperparameter search values

--- theano_models/SdA/hyperparam_search/submit_hypersearch_corr.sh
+++ theano_models/SdA/hyperparam_search/submit_hypersearch_corr.sh
@@ -16,7 +16,7 @@
 
 # To make substitutions from a higher up script: -a $FIRSTMODEL -b $SECONDMODEL -o $OFFSET
 cd $PBS_O_WORKDIR
-python hyperparam_multiproc.py -d "${SCRATCH}/gpu_models/SdA/hyper_opt/" -s corruption -a $FIRSTMODEL -b $SECONDMODEL -i "${SCRATCH}/sm_rep1_data/sm_rep1_screen.h5" -o $OFFSET
+python hyperparam_multiproc.py -d "${SCRATCH}/gpu_models/SdA/hyper_opt/2013-06-21" -s corruption -a $FIRSTMODEL -b $SECONDMODEL -i "${SCRATCH}/sm_rep1_data/sm_rep1_screen.h5" -o $OFFSET
 
 
 
