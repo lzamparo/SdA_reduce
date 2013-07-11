10044aa31ac7f1dc4b4d216c102d8afcd3885950
Lee Zamparo <zamparo@gmail.com>
2013-07-11 12:13:08 -0400

    Added scripts to produce reduced data for a colletion of SdA  models.

---
+++ theano_models/SdA/model_reduce_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Batch qsub submission script to produce reduced data for each model of SdA
+
+arr=(`ls $SCRATCH/gpu_models/SdA/finetune_pkl_files/batch_100_size_25ch_mom_0.8_wd_0.00001_lr_0.005`)
+len=${#arr[*]}
+
+for((i=1; i<=$len; i+=2 ))
+do
+    let prev=$i-1
+    first=${arr[$i]}
+    second=${arr[$prev]}
+    qsub submit_finetune_gravity.sh -v FIRSTMODEL="$first",SECONDMODEL="$second"
+    sleep 5   
+done
