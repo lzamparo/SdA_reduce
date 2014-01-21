318461789b47513545b45ac5b1023784c1c2ad7a
Lee Zamparo <zamparo@gmail.com>
2014-01-21 12:44:16 -0500

    Changed finetuning submission script to account for new directory structure

--- theano_models/SdA/submit_finetune_gravity.sh
+++ theano_models/SdA/submit_finetune_gravity.sh
@@ -16,7 +16,7 @@
 
 # To make substitutions from a higher up script: -p $FIRSTMODEL -q $SECONDMODEL -o $OFFSET
 cd $PBS_O_WORKDIR
-python finetune_SdA_multiproc.py -d "${SCRATCH}/gpu_models/SdA/finetune_output" -e "batch_100_size_25ch_mom_0.8_wd_0.00001_lr_0.005" -x "batch_1000_size_25ch_mom_0.8_wd_0.00001_lr_0.005" -p $FIRSTMODEL -q $SECONDMODEL -i "${SCRATCH}/sm_rep1_data/sm_rep1_screen.h5" -o $OFFSET
+python finetune_SdA_multiproc.py -d "${SCRATCH}/gpu_models/SdA/finetune_output" -e "40" -x "40" -p $FIRSTMODEL -q $SECONDMODEL -i "${SCRATCH}/sm_rep1_data/sm_rep1_screen.h5" -o $OFFSET
 
 
 
