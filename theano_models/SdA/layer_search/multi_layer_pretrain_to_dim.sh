4a7797b58ecac5ac870ab64ff0a668364c5a343c
Lee Zamparo <zamparo@gmail.com>
2014-03-18 12:42:58 -0400

    added final layer size parameter to model_gridsearch.sh, added 3,5 layer pretraining batch job submit script

---
+++ theano_models/SdA/layer_search/multi_layer_pretrain_to_dim.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Batch qsub submission script for pre-training 3,5 layer models with architectures specified in the following lists 
+# with a given final layer size parameter.
+
+offset=0
+
+threeLayerModelList=(700_100_ 700_200_ 600_300_ 900_400_
+700_400_ 500_400_ 500_300_ 700_300_ 700_250_
+700_350_ 700_450_ 500_300_ 900_300_ 900_350_
+900_200_ 600_400_ 700_100_ 400_300_ 900_100_
+650_400_)
+
+fiveLayerModelList=(700_800_700_100_ 1000_850_700_200_ 1000_800_600_300_ 800_850_900_400_
+1000_850_700_400_ 700_600_500_400_ 700_600_500_300_ 700_750_700_300_ 900_800_700_200_
+700_800_500_300_ 700_700_400_200_ 900_600_500_300_ 800_900_500_300_ 1000_900_500_300_
+900_800_400_200_ 700_600_400_200_ 900_700_300_100_ 900_700_400_300_ 700_900_400_100_
+900_700_400_200_)
+
+outputLayer=$1
+
+len=${#threeLayerModelList[*]}
+
+for((i=1; i<=$len; i+=2 ))
+do
+    let prev=$i-1
+    first="${threeLayerModelList[$i]}$outputLayer"
+    second="${threeLayerModelList[$prev]}$outputLayer"
+    qsub submit_pretrain_gravity_3layers.sh -v FIRSTMODEL="$first",SECONDMODEL="$second",OFFSET="$offset",OUTDIRSUFFIX="$outputLayer"
+    ((offset+=5))
+    sleep 5
+    
+    # Each pair of jobs needs 30 data chunks, and there are 211 in total.  Reset the offset parameter if necessary.
+    if (( offset > 181 )); then 
+       offset=0
+    fi    
+done
+
+len=${#fiveLayerModelList[*]}
+
+for((i=1; i<=$len; i+=2 ))
+do
+    let prev=$i-1
+    first="${fiveLayerModelList[$i]}$outputLayer"
+    second="${fiveLayerModelList[$prev]}$outputLayer"
+    qsub submit_pretrain_gravity_5layers.sh -v FIRSTMODEL="$first",SECONDMODEL="$second",OFFSET="$offset",OUTDIRSUFFIX="$outputLayer"
+    ((offset+=5))
+    sleep 5
+    
+    # Each pair of jobs needs 30 data chunks, and there are 211 in total.  Reset the offset parameter if necessary.
+    if (( offset > 181 )); then 
+       offset=0
+    fi    
+done
--- theano_models/SdA/model_gridsearch.sh
+++ theano_models/SdA/model_gridsearch.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-# Batch qsub submission script for model search over SdA layer sizes 
+# Batch qsub submission script for model search over SdA layer sizes
+# with a given output dimension
 
 offset=0
+outputLayer=$1
+
 for i in {700..1000..100}; # first layer
   do
   for j in {500..900..100}; # second layer
     do 
       # Fix the third layer as 100, 200, 300, 400
       # Submit the first job
-      first="$i-$j-400-50"  
-      second="$i-$j-300-50"
+      first="$i-$j-400-$outputLayer"  
+      second="$i-$j-300-$outputLayer"
       ((offset++))
       qsub submit_pretrain_gravity.sh -v FIRSTMODEL="$first",SECONDMODEL="$second",OFFSET="$offset"
       sleep 5
       
       # Submit the next job
-      first="$i-$j-200-50"  
-      second="$i-$j-100-50"
+      first="$i-$j-200-$outputLayer"  
+      second="$i-$j-100-$outputLayer"
       ((offset++))
       qsub submit_pretrain_gravity.sh -v FIRSTMODEL="$first",SECONDMODEL="$second",OFFSET="$offset"
       
