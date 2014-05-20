15edc44688951d4097234233b6d63ae7b7493724
Lee Zamparo <zamparo@gmail.com>
2014-05-20 01:28:26 -0400

    Ready to submit all reduction jobs

--- theano_models/SdA/layer_search/submit_all_reduce.sh
+++ theano_models/SdA/layer_search/submit_all_reduce.sh
@@ -19,7 +19,7 @@ do
             let prev=$i-1
             first=${three_arr[$i]}
             second=${three_arr[$prev]}
-            echo "submit_reduce_gravity_3layers.sh -v FIRSTMODEL="$first",SECONDMODEL="$second",OFFSET="$offset",EXTENSION="$dim/$layertype""            
+            qsub submit_reduce_gravity_3layers.sh -v FIRSTMODEL="$first",SECONDMODEL="$second",OFFSET="$offset",EXTENSION="$dim/$layertype"            
             ((offset+=5))
             sleep 5
             
@@ -34,34 +34,34 @@ do
 done
 
 # Submit 5 layer jobs
-#cd "$SCRATCH/gpu_tests/5_layers/finetune_pkl_files" 
+cd "$SCRATCH/gpu_tests/SdA_results/5_layers/finetune_pkl_files" 
 
-#for dim in {10..30..10}
-#do
-    #cd "$dim"
-    #for layertype in {"gb","relu"}
-    #do
-        #cd "$layertype"
-        #three_arr=(`ls .`)
-        #offset=0
-        #len=${#three_arr[*]}
-        #for((i=1; i<=$len; i+=2 ))
-        #do
-            #let prev=$i-1
-            #first=${three_arr[$i]}
-            #second=${three_arr[$prev]}
-            #qsub submit_reduce_gravity_5layers.sh -v FIRSTMODEL="$first",SECONDMODEL="$second",OFFSET="$offset",EXTENSION="$dim/$layertype"
-            #((offset+=5))
-            #sleep 5
+for dim in {10..30..10}
+do
+    cd "$dim"
+    for layertype in {"gb","relu"}
+    do
+        cd "$layertype"
+        five_arr=(`ls .`)
+        offset=0
+        len=${#five_arr[*]}
+        for((i=1; i<=$len; i+=2 ))
+        do
+            let prev=$i-1
+            first=${five_arr[$i]}
+            second=${five_arr[$prev]}
+            qsub submit_reduce_gravity_5layers.sh -v FIRSTMODEL="$first",SECONDMODEL="$second",OFFSET="$offset",EXTENSION="$dim/$layertype"
+            ((offset+=5))
+            sleep 5
             
-            ## Each pair of jobs needs 30 data chunks, and there are 211 in total.  Reset the offset parameter if necessary.
-            #if (( offset > 181 )); then 
-               #offset=0
-            #fi    
-        #done            
-        #cd ..
-    #done
-    #cd ..
-#done
+            # Each pair of jobs needs 30 data chunks, and there are 211 in total.  Reset the offset parameter if necessary.
+            if (( offset > 181 )); then 
+               offset=0
+            fi    
+        done            
+        cd ..
+    done
+    cd ..
+done
 
 
