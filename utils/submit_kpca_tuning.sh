6be0b7f0a87114285786e66943159a52ec901e99
Lee Zamparo <zamparo@gmail.com>
2014-05-20 11:05:15 -0400

    fixed path error for reduction scripts.  Finetuning scripts are same but for different Kpca tuning script just along for the ride.

---
+++ utils/submit_kpca_tuning.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Kernel PCA tuning script for GPC
+#
+# N.B: PBS lines are interpreted by qsub.  Change these defaults as 
+# required
+#
+#PBS -l nodes=1:ppn=8,walltime=12:00:00
+#PBS -N kpca_tuning
+
+# Load theano modules
+#cd ~/jobscripts
+#sh load_theano_modules.sh
+
+# Run the job
+cd $PBS_O_WORKDIR
+python kernel_pca_pipeline.py --h5file "${SCRATCH}/sm_rep1_data/sample.h5" --size 7 --sample-size 5000 --num-jobs 8 --output "${SCRATCH}/figures/kpca_pipeline.pkl" 
