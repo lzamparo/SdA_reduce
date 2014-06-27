0886e51ec1fa09ff0fda31c94acce913afbc002d
Lee Zamparo <zamparo@gmail.com>
2014-06-27 12:16:01 -0400

    Adding changes to dA test script

--- theano_models/dA/submit_dA_foci.sh
+++ theano_models/dA/submit_dA_foci.sh
@@ -4,9 +4,9 @@
 # N.B: PBS lines are interpreted by qsub.  Change these defaults as 
 # required
 #
-#PBS -l nodes=1:ppn=8:gpus=1,walltime=2:00:00
+#PBS -l nodes=1:ppn=12:gpus=1,walltime=2:00:00
 #PBS -N relu_vs_gb_dA
-#PBS -q arc
+#PBS -q gravity
 
 # Load theano modules
 #cd ~/jobscripts
@@ -14,4 +14,4 @@
 
 # Run the job
 cd $PBS_O_WORKDIR
-python ReLU_vs_GB_test_script.py -d "${SCRATCH}/gpu_tests/dA_results/" -i "${SCRATCH}/sm_rep1_data/sample.h5" -c $CORRUPTION
+python test_dA.py -d "${SCRATCH}/gpu_tests/dA_results/" -i "${SCRATCH}/sm_rep1_data/sample.h5" -c $CORRUPTION
