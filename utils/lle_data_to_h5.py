91b965195af02518419461f1e07b1cdc40b893c5
Lee Zamparo <zamparo@gmail.com>
2014-09-08 17:18:34 -0400

    similar scripts to the isomap data to h5 script

---
+++ utils/lle_data_to_h5.py
@@ -0,0 +1,68 @@
+from sklearn.preprocessing import scale
+from sklearn.manifold import LocallyLinearEmbedding
+
+import logging, os
+from optparse import OptionParser
+import sys
+
+import numpy as np
+from tables import *
+from extract_datasets import extract_labeled_chunkrange
+
+
+# Display progress logs on stdout
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s')
+
+# parse commandline arguments
+op = OptionParser()
+op.add_option("--h5file",
+              dest="inputfile", help="Read data input from this hdf5 file.")
+op.add_option("--output",
+              dest="outputfile", help="Write matrix to .npy output file.")
+op.add_option("--size",
+              dest="size", type="int", help="Extract the first size chunks of the data set and labels.")
+
+(opts, args) = op.parse_args()
+
+
+np.random.seed(0)
+
+###############################################################################
+# Load a training set from the given .h5 file
+datafile = openFile(opts.inputfile, mode = "r", title = "Data is stored here")
+
+# Extract some of the dataset from the datafile
+X, labels = extract_labeled_chunkrange(datafile, opts.size)
+
+# Remove the first four indexing features introduced by CP
+X = X[:,5:-1]
+
+filename = opts.outputfile
+h5file = openFile(filename, mode = "w", title = "Reduced Data File")
+
+# Create a new group under "/" (root)
+arrays_group = h5file.createGroup("/", 'recarrays', 'The object data arrays')
+zlib_filters = Filters(complib='zlib', complevel=5)
+
+# done, close h5 files
+datafile.close()
+
+###############################################################################
+
+D = scale(X[:,0:612])
+
+n_samples, n_features = D.shape
+
+# chosen by hyperparam search in a separate test.
+n_neighbors = 35
+
+for i in [10,20,30,40,50]:     
+    lle = LocallyLinearEmbedding(n_neighbors, n_components=i, method='standard')
+    X_lle = lle.fit_transform(D)
+    atom = Atom.from_dtype(X_lle.dtype)
+    ds = h5file.createCArray(where=arrays_group, name=str(i), atom=atom, shape=X_lle.shape, filters=zlib_filters)
+    ds[:] = X_lle
+    h5file.flush()    
+    
+h5file.close()
