fbed648d04e3b436ac749a27795fbe85d5c6e38c
Lee Zamparo <zamparo@gmail.com>
2015-03-10 16:29:04 -0400

    fixed to run on old api of pytables, in gnu-parallel jobs

--- plot_scripts/full_homog_adjrand_test.py
+++ plot_scripts/full_homog_adjrand_test.py
@@ -6,6 +6,7 @@ from tables import *
 from extract_datasets import extract_labeled_chunkrange
 from time import time
 import cPickle as pkl
+import os
 
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 p = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
@@ -13,13 +14,10 @@ p = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 p.add_argument('-i', metavar='<inputfile>',
                default='/scratch/z/zhaolei/lzamparo/sm_rep1_data/sample.h5',
                 help='input data')
-p.add_argument('-o', metavar='<savefile>',
-               default='/scratch/z/zhaolei/lzamparo/figures/full_dim_homog_test.pkl',
-               help='load the dpmm from this pkl file')
 args = p.parse_args()
 
 
-datafile = open_file(args.i,'r')
+datafile = openFile(args.i,'r')
 X, labels = extract_labeled_chunkrange(datafile, 3)
 X = X[:,5:-1]
 import numpy as np
@@ -27,21 +25,12 @@ true_k =  np.unique(labels[:,0]).shape[0]
 datafile.close()
 D = scale(X)
 
-full_dim_homog = []
-full_dim_adjrand = []
-time_stats = []
-
-for j in xrange(10):
-    gaussmix = GMM(n_components=true_k, covariance_type='tied', n_init=10, n_iter=1000)
-    t0 = time()
-    gaussmix.fit(D)
-    print "GMM fit, took %.2fs" % (time() - t0)
-    time_stats.append(time() - t0)
-    gaussmix_labels = gaussmix.predict(D)
-    print "Homogeneity %0.3f" % metrics.homogeneity_score(labels[:,0], gaussmix_labels)
-    print "Adjusted Rand Index %0.3f" % metrics.adjusted_rand_score(labels[:,0],gaussmix_labels)
-    full_dim_homog.append(metrics.homogeneity_score(labels[:,0], gaussmix_labels))
-    full_dim_adjrand.append(metrics.adjusted_rand_score(labels[:,0],gaussmix_labels))
+gaussmix = GMM(n_components=true_k, covariance_type='tied', n_init=10, n_iter=1000)
+t0 = time()
+gaussmix.fit(D)
+print "[Worker %d] GMM fit, took %.2fs" % (os.getpid(), time() - t0)
+time_stats.append(time() - t0)
+gaussmix_labels = gaussmix.predict(D)
+print "[Worker %d] Homogeneity %0.3f" % (os.getpid(), metrics.homogeneity_score(labels[:,0], gaussmix_labels))
+print "[Worker %d] Adjusted Rand Index %0.3f" % (os.getpid(), metrics.adjusted_rand_score(labels[:,0],gaussmix_labels))
     
-test_stats = {'time': time_stats, 'homogeneity': full_dim_homog, 'adjusted_rand_index': full_dim_adjrand}
-pkl.dump(test_stats, open(args.o,'wb'), protocol=0)
